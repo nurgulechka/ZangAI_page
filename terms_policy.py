470942892d6c7ca0b0c6bd0c1670771ea7b3ce90
nurgul <nu.kozhakhmetova@gmail.com>
2023-08-05 21:26:38 +0600

    terms and policy

---
+++ terms_policy.py
@@ -0,0 +1,144 @@
+import streamlit as st
+
+
+def privacy_policy():
+    st.title("ZangAI App Privacy Policy")
+    st.write("Effective Date: 28.07.2023")
+    st.subheader("Privacy Policy")
+    st.write(
+        "ZangAI built the ZangAI app as a Free app. This SERVICE is provided by ZangAI at no cost and is intended for use as is."
+    )
+    st.write(
+        "This page is used to inform visitors regarding my policies with the collection, use, and disclosure of Personal Information if anyone decided to use my Service."
+    )
+    st.write(
+        "If you choose to use my Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that I collect is used for providing and improving the Service. I will not use or share your information with anyone except as described in this Privacy Policy."
+    )
+    st.write(
+        "The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at ZangAI unless otherwise defined in this Privacy Policy."
+    )
+
+    st.subheader("Information Collection and Use")
+    st.write(
+        "For a better experience, while using our Service, I may require you to provide us with certain personally identifiable information. The information that I request will be retained on your device and is not collected by me in any way."
+    )
+
+    st.subheader("Log Data")
+    st.write(
+        "I want to inform you that whenever you use my Service, in a case of an error in the app I collect data and information (through third-party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing my Service, the time and date of your use of the Service, and other statistics."
+    )
+
+    st.subheader("Cookies")
+    st.write(
+        "Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory."
+    )
+    st.write(
+        "This Service does not use these “cookies” explicitly. However, the app may use third-party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service."
+    )
+
+    st.subheader("Service Providers")
+    st.write(
+        """
+        We may employ third-party companies and individuals due to the following reasons:
+
+            - To facilitate our Service;
+            - To provide the Service on our behalf;
+            - To perform Service-related services; or
+            - To assist us in analyzing how our Service is used.
+            
+        We want to inform users of this Service that these third parties have access to their Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.
+        """
+    )
+
+    st.subheader("Security")
+    st.write(
+        "We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security."
+    )
+
+    st.subheader("Links to Other Sites")
+    st.write(
+        "This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services."
+    )
+
+    st.subheader("Children’s Privacy")
+    st.write(
+        "We do not knowingly collect personally identifiable information from children. We encourage all children to never submit any personally identifiable information through the Application and/or Services. We encourage parents and legal guardians to monitor their children's Internet usage and to help enforce this Policy by instructing their children never to provide personally identifiable information through the Application and/or Services without their permission."
+    )
+    st.write(
+        "If you have reason to believe that a child has provided personally identifiable information to us through the Application and/or Services, please contact us. You must also be at least 16 years of age to consent to the processing of your personally identifiable information in your country (in some countries we may allow your parent or guardian to do so on your behalf)."
+    )
+    st.write(
+        "This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services."
+    )
+
+    st.subheader("Changes to This Privacy Policy")
+    st.write(
+        "We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page."
+    )
+    st.write("This policy is effective as of 2023-08-05")
+
+    st.subheader("Contact Us")
+    st.write(
+        "If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at nu.kozhakhmetova@gmail.com."
+    )
+
+
+def terms_conditions():
+    st.title("ZangAI App Terms & Conditions")
+    st.write("Effective Date: 28.07.2023")
+    st.subheader("Terms & Conditions")
+    st.write(
+        "By downloading or using the app, these terms will automatically apply to you – you should make sure therefore that you read them carefully before using the app. You’re not allowed to copy or modify the app, any part of the app, or our trademarks in any way. You’re not allowed to attempt to extract the source code of the app, and you also shouldn’t try to translate the app into other languages or make derivative versions. The app itself, and all the trademarks, copyright, database rights, and other intellectual property rights related to it, still belong to ZangAI."
+    )
+    st.write(
+        "ZangAI is committed to ensuring that the app is as useful and efficient as possible. For that reason, we reserve the right to make changes to the app or to charge for its services, at any time and for any reason. We will never charge you for the app or its services without making it very clear to you exactly what you’re paying for."
+    )
+    st.write(
+        "The ZangAI app stores and processes personal data that you have provided to us, to provide our Service. It’s your responsibility to keep your phone and access to the app secure. We therefore recommend that you do not jailbreak or root your phone, which is the process of removing software restrictions and limitations imposed by the official operating system of your device. It could make your phone vulnerable to malware/viruses/malicious programs, compromise your phone’s security features and it could mean that the ZangAI app won’t work properly or at all."
+    )
+    st.write(
+        "You should be aware that there are certain things that ZangAI will not take responsibility for. Certain functions of the app will require the app to have an active internet connection. The connection can be Wi-Fi or provided by your mobile network provider, but ZangAI cannot take responsibility for the app not working at full functionality if you don’t have access to Wi-Fi, and you don’t have any of your data allowance left."
+    )
+    st.write(
+        "If you’re using the app outside of an area with Wi-Fi, you should remember that the terms of the agreement with your mobile network provider will still apply. As a result, you may be charged by your mobile provider for the cost of data for the duration of the connection while accessing the app, or other third-party charges. In using the app, you’re accepting responsibility for any such charges, including roaming data charges if you use the app outside of your home territory (i.e. region or country) without turning off data roaming. If you are not the bill payer for the device on which you’re using the app, please be aware that we assume that you have received permission from the bill payer for using the app."
+    )
+    st.write(
+        "Along the same lines, ZangAI cannot always take responsibility for the way you use the app i.e. You need to make sure that your device stays charged – if it runs out of battery and you can’t turn it on to avail the Service, ZangAI cannot accept responsibility."
+    )
+    st.write(
+        "With respect to ZangAI’s responsibility for your use of the app, when you’re using the app, it’s important to bear in mind that although we endeavor to ensure that it is updated and correct at all times, we do rely on third parties to provide information to us so that we can make it available to you. ZangAI accepts no liability for any loss, direct or indirect, you experience as a result of relying wholly on this functionality of the app."
+    )
+    st.write(
+        "At some point, we may wish to update the app. The app is currently available on iOS – the requirements for the system(and for any additional systems we decide to extend the availability of the app to) may change, and you’ll need to download the updates if you want to keep using the app. ZangAI does not promise that it will always update the app so that it is relevant to you and/or works with the iOS version that you have installed on your device. However, you promise to always accept updates to the application when offered to you, We may also wish to stop providing the app, and may terminate use of it at any time without giving notice of termination to you. Unless we tell you otherwise, upon any termination, (a) the rights and licenses granted to you in these terms will end; (b) you must stop using the app, and (if needed) delete it from your device."
+    )
+
+    st.subheader("Changes to This Terms and Conditions")
+    st.write(
+        "We may update our Terms and Conditions from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Terms and Conditions on this page."
+    )
+    st.write("These terms and conditions are effective as of 2023-08-05")
+
+
+def contact_page():
+    st.title("ZangAI App Contact & Support")
+    st.write(
+        "Welcome to the Contact & Support Page. If you have any questions or need assistance, feel free to reach out to us at nu.kozhakhmetova@gmail.com."
+    )
+    st.write("These terms and conditions are effective as of 2023-08-05")
+
+
+def main():
+    st.sidebar.title("Navigation")
+    page_options = ["Privacy Policy", "Terms & Conditions", "Contact & Support"]
+    selected_page = st.sidebar.radio("Select a page", page_options)
+
+    if selected_page == "Privacy Policy":
+        privacy_policy()
+    elif selected_page == "Terms & Conditions":
+        terms_conditions()
+    elif selected_page == "Contact & Support":
+        contact_page()
+
+
+if __name__ == "__main__":
+    main()
